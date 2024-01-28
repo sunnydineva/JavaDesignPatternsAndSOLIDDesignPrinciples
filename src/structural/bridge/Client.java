c814d2da2bfd1dee348470869039b95ddac394bd
Sunny Dineva <sunny.dineva@uptempo.io>
2024-01-28 21:47:12 +0200

    Bridge Design Pattern

---
+++ src/structural/bridge/Client.java
@@ -0,0 +1,18 @@
+package structural.bridge;
+public class Client {
+
+	public static void main(String[] args) {
+		FifoCollection<Integer> collection = new Queue<>(new SinglyLinkedList<>());
+		collection.offer(10);
+		collection.offer(20);
+		collection.offer(30);
+
+		System.out.println(collection.poll());
+		System.out.println(collection.poll());
+		System.out.println(collection.poll());
+
+		System.out.println(collection.poll());
+
+	}
+
+}
